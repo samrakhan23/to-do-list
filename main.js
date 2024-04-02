bd4ee4df52e709521c558c912f7c29ac8baa6342
samrakhan23 <samrakhalil93@gmail.com>
2024-04-02 22:50:06 +0500

    Push my to do list to github

---
+++ main.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+let toDos = [];
+let condition = true;
+while (condition) {
+    let toDo = await inquirer.prompt([
+        {
+            name: "todolist",
+            message: "What do you want to add in your To Do List?",
+            type: "input",
+        },
+        {
+            name: "answer",
+            type: "confirm",
+            message: "Are you sure want to add more todos?",
+            default: "false"
+        },
+    ]);
+    toDos.push(toDo.todolist);
+    condition = toDo.answer;
+    console.log(toDos);
+}
